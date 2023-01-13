d94eec7551bcdcb00a23311010967990f83226b2
DenChernomorets <den.chernomorets@gmail.com>
2023-01-13 16:02:08 +0300

    1

--- Homework 6/tests.py
+++ Homework 6/tests.py
@@ -1,1 +1,80 @@
-import pytest
+
+from validators import check_pin, check_mail, check_name, check_pass
+
+def test_check_pin():
+    '''
+    False tests
+    '''
+    assert check_pin('1234') == False
+    assert check_pin('1111') == False
+    assert check_pin('qwe') == False
+    assert check_pin('112') == False
+    assert check_pin('') == False
+    assert check_pin('00001') == False
+    assert check_pin('-123') == False
+    '''
+    True tsts
+    '''
+    assert check_pin('3332') == True
+    assert check_pin('1100') == True
+    assert check_pin('8954') == True
+
+def test_check_mail():
+    '''
+    False tests
+    '''
+    assert check_mail('alabuga.ru') == False
+    assert check_mail('alabugaru') == False
+    assert check_mail('alabuga@ru') == False
+    assert check_mail('alabugaru@') == False
+    assert check_mail('alabugaru.') == False
+    assert check_mail('@.') == False
+    assert check_mail('@alabuga') == False
+    assert check_mail('.alabuga') == False
+    assert check_mail('@ga.ru') == False
+    assert check_mail('alabu@.ru') == False
+    assert check_mail('alabu@ga.') == False
+    '''
+    True tests
+    '''
+    assert check_mail('alabu@ga.ru') == True
+    
+def test_check_name():
+    '''
+    False test
+    '''
+    assert check_name('Denis') == False
+    assert check_name('Денис_Черноморец') == False
+    assert check_name('Денис+Черноморец') == False
+    assert check_name('Денис Chernomorets') == False
+    assert check_name('Денис123') == False
+    '''
+    True tests
+    '''
+    assert check_name('Денис Черноморец') == False
+    assert check_name('Денис') == False
+    assert check_name('ДенисЧерноморец') == False
+    assert check_name('ДеНиС') == False
+    assert check_name('денис') == False
+
+
+def test_check_pass():
+    '''
+    False tests
+    '''
+    assert check_pass('123') == False
+    assert check_pass('123qwe') == False
+    assert check_pass('verylongpass') == False
+    assert check_pass('12313144123') == False
+    assert check_pass('qwe') == False
+    assert check_pass('qwertyuio1234_}[]";') == False
+    assert check_pass('') == False
+    '''
+    True tests
+    '''
+    assert check_pass('longpass12341') == True
+    assert check_pass('12341longpass') == True
+    assert check_pass('LoNlOjHg120Hg1') == True
+    assert check_pass('qiiyh4912n3') == True
+    assert check_pass('KJHIHBJ71BHj1') == True
+    assert check_pass('1Juubhakqwda') == True
--- Homework 6/validators.py
+++ Homework 6/validators.py
@@ -7,13 +7,50 @@ check_pass() - проверяет, чтобы пароль был не мень�
 check_mail() - проверяет наличие собаки и точки
 check_name() - проверяет содержание в имени только русских букв и пробелов.
 '''
-
+'''
 import re
-# not pin[0] == pin[1] == ...
+
 def check_pin(user_pin):
     try:
         if int(user_pin) > 999 and int(user_pin) < 10000 and str(user_pin) == user_pin:
-            if user_pin[0] != user_pin[1] or user_pin[0] != user_pin[2] or user_pin[0] != user_pin[3]:
+            if user_pin[0] != user_pin[1]:
+                if int(user_pin) != 1234:
+                    return True
+    except:
+        return False
+    return False
+
+def check_pin_regular(user_pin):
+    regular_pin = r'\b\d{4}\b'
+    if re.match(regular_pin, user_pin):
+        return True
+    else:
+        return False
+'''
+def check_pass(user_pass):
+    '''
+    check_pass() - проверяет, чтобы пароль был не меньше 9 символов,
+    содержал буквы и цифры
+    '''
+    pass
+
+def check_mail(user_mail):
+    '''
+    check_mail() - проверяет наличие собаки и точки
+    '''
+    pass
+
+def check_name(user_name):
+    '''
+    check_name() - проверяет содержание в имени только русских букв и пробелов.
+    '''
+    pass
+
+
+def check_pin(user_pin):
+    try:
+        if int(user_pin) > 999 and int(user_pin) < 10000 and str(user_pin) == user_pin:
+            if not user_pin[0] == user_pin[1] == user_pin[2] == user_pin[3]:
                 if int(user_pin) != 1234:
                     return True
     except:
@@ -21,46 +58,9 @@ def check_pin(user_pin):
     return False
 
 
-#(?!2222)(?!3333)(?!4444)(?!5555)(?!6666)(?!7777)(?!8888)(?!9999)(?!0000)(?!1234)
-def check_pin_regular(user_pin):
-    regular_pin = r'(?!1{4})(?!2{4})(?!3{4})(?!4{4})(?!5{4})(?!6{4})(?!7{4})(?!8{4})(?!9{4})(?!0{4})(?!1234)\b\d{4}\b'
-    if re.match(regular_pin, user_pin):
-        return True
-    else:
-        return False
-
-
-def check_pass(user_pass):
-    flag1 = False
-    flag2 = False
-    if len(user_pass) > 8:
-        for letter in user_pass:
-            if ord(letter) > 47 and ord(letter) < 58: # a in b
-                flag1 = True
-            elif ord(letter) > 64 and ord(letter) < 91 or ord(letter) > 96 and ord(letter) < 123: # a in b
-                flag2 = True
-    if flag1 and flag2:
-        return True
-    else:
-        return False
-
-
-def check_pass_regular(user_pass):
-    regular_pass = r'\b*\d*\w\b'
-    if re.match(regular_pass, user_pass):
-        return True
-    else:
-        return False
-
-
-#def check_mail(user_mail):
-
-#def check_name(user_name):
-
-
 def main():
     while True:
-        print(check_pass_regular(input()))
+        print(check_pin(input()))
 
 
 if __name__ == '__main__':
